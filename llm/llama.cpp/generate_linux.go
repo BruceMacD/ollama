9d0164bfa76a46119d1ade961b11f3506746c96b
Bruce MacDonald <brucewmacdonald@gmail.com>
2023-09-11 15:16:41 -0400

    multiple cuda versions

--- llm/llama.cpp/generate_linux.go
+++ llm/llama.cpp/generate_linux.go
@@ -9,4 +9,10 @@ package llm
 
 //go:generate git submodule update --force gguf
 
+//go:generate cmake -S ggml -B ggml/build/cpu -DLLAMA_K_QUANTS=on
+//go:generate cmake --build ggml/build/cpu --target server --config Release
+
+//go:generate cmake -S gguf -B gguf/build/cpu -DLLAMA_K_QUANTS=on
+//go:generate cmake --build gguf/build/cpu --target server --config Release
+
 //go:generate sh generate_linux.sh
--- llm/llama.cpp/generate_linux.sh
+++ llm/llama.cpp/generate_linux.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-# only do GPU builds if nvcc is installed
-if which nvcc > /dev/null; then
-  # get CUDA version information
-  CUDA_MAJOR_VERSION=$(nvcc --version | grep -o 'cuda_[0-9]*\.[0-9]*' | awk -F'_' '{print $2}' | awk -F'.' '{print $1}')
-  
-  cmake -S ggml -B ggml/build/cuda-$CUDA_MAJOR_VERSION -DLLAMA_CUBLAS=on -DLLAMA_ACCELERATE=on -DLLAMA_K_QUANTS=on
-  cmake --build ggml/build/cuda-$CUDA_MAJOR_VERSION --target server --config Release
+# Find the latest CUDA 11 nvcc
+latest_cuda_11=$(ls /usr/local | grep -E 'cuda-11\.[0-9]+$' | sort -V | tail -n 1)
 
-  cmake -S gguf -B gguf/build/cuda-$CUDA_MAJOR_VERSION -DLLAMA_CUBLAS=on -DLLAMA_ACCELERATE=on -DLLAMA_K_QUANTS=on
-  cmake --build gguf/build/cuda-$CUDA_MAJOR_VERSION --target server --config Release
-else
-  echo "Warning: nvcc is not installed, skipping CUDA builds."
+# Find the latest CUDA 12 nvcc
+latest_cuda_12=$(ls /usr/local | grep -E 'cuda-12\.[0-9]+$' | sort -V | tail -n 1)
+
+if [ ! -z "$latest_cuda_11" ]; then
+  cuda_11_nvcc_path="/usr/local/$latest_cuda_11/bin/nvcc"
+  echo "Found CUDA 11 nvcc at $cuda_11_nvcc_path"
+
+  cmake -S ggml -B ggml/build/cuda-11 -DLLAMA_CUBLAS=on -DLLAMA_ACCELERATE=on -DLLAMA_K_QUANTS=on
+  cmake --build ggml/build/cuda-11 --target server --config Release
+
+  cmake -S gguf -B gguf/build/cuda-11 -DLLAMA_CUBLAS=on -DLLAMA_ACCELERATE=on -DLLAMA_K_QUANTS=on
+  cmake --build gguf/build/cuda-11 --target server --config Release
+fi
+
+if [ ! -z "$latest_cuda_12" ]; then
+  cuda_12_nvcc_path="/usr/local/$latest_cuda_12/bin/nvcc"
+  echo "Found CUDA 12 nvcc at $cuda_12_nvcc_path"
+
+  cmake -S ggml -B ggml/build/cuda-12 -DLLAMA_CUBLAS=on -DLLAMA_ACCELERATE=on -DLLAMA_K_QUANTS=on
+  cmake --build ggml/build/cuda-12 --target server --config Release
+
+  cmake -S gguf -B gguf/build/cuda-12 -DLLAMA_CUBLAS=on -DLLAMA_ACCELERATE=on -DLLAMA_K_QUANTS=on
+  cmake --build gguf/build/cuda-12 --target server --config Release
 fi
